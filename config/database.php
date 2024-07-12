ccb414eccba117916ef2dbd1dabfbde54dde9b57
mdmontes94 <156222980+mdmontes94@users.noreply.github.com>
2024-07-11 21:51:01 -0500

    Added function to connect to database

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+function getConnection() {
+    $dataURL = 'mysql:host=localhost;dbname=music_site';
+    $user = 'AHon209';
+    $pass = '5outh3ast';
+
+    try {
+        $db = new PDO($dataURL, $user, $pass);
+        return $db;
+    } 
+    catch(PDOException $e) {
+        $error = $e->getMessage();
+        echo "<p>Connection Error: $error </p>";
+        return null;
+    }
+}
+?>
