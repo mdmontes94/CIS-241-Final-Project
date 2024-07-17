a82675119797b32109129ed93cdf4a9c4b4df8ec
Mario Montes <mdmontes94@gmail.com>
2024-07-16 19:34:40 -0500

    Defined $action and added case main_page

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
+$action = $_GET['action'] ?? 'main_page';
+
 switch ($action) {
+    case 'main_page':
+        include('views/main_page.php');
+        break;
+    
     case 'list':
         include('views/list_view.php');
         break;
