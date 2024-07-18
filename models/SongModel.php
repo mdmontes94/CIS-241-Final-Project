5a04bad4bb9074bf0da38cbd2c2abd3dbfb15910
Mario Montes <mdmontes94@gmail.com>
2024-07-18 16:05:07 -0500

    Created delete and update functions for SongModel.php

--- models/SongModel.php
+++ models/SongModel.php
@@ -51,6 +51,38 @@ class SongModel {
         }
     }
 
+    public function deleteSong($song_id) {
+        if($song_id !== false) {
+            $deleteQuery = 'DELETE FROM songs
+                            WHERE song_id = :song_id';
+            $statement = $this->conn->prepare($deleteQuery);
+            $statement->bindValue(':song_id', $song_id);
+            $success = $statement->execute();
+            $statement->closeCursor();
+
+            return $success;
+        }
+    }
+
+    public function updateSong($song_id, $song_title, $artist_id, $album_id, $genre_id, $length, $year_released) {
+        if($song_id !== false && $song_title && false && $artist_id !== false) {
+            $updateQuery = 'UPDATE songs 
+                            SET song_title = :song_title, artist_id = :artist_id, album_id = :album_id, genre_id = :genre_id, length = :length, year_released = :year_released
+                            WHERE song_id = :song_id';
+            $statement = $this->conn->prepare($updateQuery);
+            $statement->bindValue(':song_id', $song_id);
+            $statement->bindValue(':song_title', $song_title); 
+            $statement->bindValue(':artist_id', $artist_id); 
+            $statement->bindValue(':album_id', $album_id); 
+            $statement->bindValue(':genre_id', $genre_id); 
+            $statement->bindValue(':length', $length);
+            $statement->bindValue(':year_released', $year_released);
+            $success = $statement->closeCursor();
+            
+            return $success;
+        }
+    }
+
 }
 
 ?>
