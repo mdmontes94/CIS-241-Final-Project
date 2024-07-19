1966ef122544918f5133681d6c1dbbeb95743f79
Mario Montes <mdmontes94@gmail.com>
2024-07-19 13:34:48 -0500

    Changed query for getSongList

--- models/SongModel.php
+++ models/SongModel.php
@@ -24,7 +24,7 @@ class SongModel {
     public function getSongList() {
         $querySongList = 'SELECT *
                           FROM songs_view
-                          ORDER BY artist_name, year_released';
+                          ORDER BY song_title';
         $statement = $this->conn->prepare($querySongList);
         $statement->execute();
         $songList = $statement->fetchAll();
