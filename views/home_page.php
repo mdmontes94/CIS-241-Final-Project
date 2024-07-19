5c1ecc20b33e486e5d3a7a65588dae715c22cccb
Mario Montes <mdmontes94@gmail.com>
2024-07-19 00:27:41 -0500

    Created .css file for home page

--- views/home_page.php
+++ views/home_page.php
@@ -1,13 +1,10 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home Page</title>
+        <link rel="stylesheet" href="./assets/home.css" />
     </head>
     <body>
         <nav class="navMenu">
@@ -16,5 +13,8 @@
             <a href="index.php?action=add_song">Add Song</a>
             <a href="index.php?action=add_artist">Add Artist</a>      
         </nav>
+        <div class="container">
+            <h1>It's a Mario's Music Manager!</h1>
+        </div>
     </body>
 </html>
