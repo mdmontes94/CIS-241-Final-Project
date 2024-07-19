3e0ad1c68769b72752db7c3a5a976863db0895be
Mario Montes <mdmontes94@gmail.com>
2024-07-18 22:39:59 -0500

    Created and linked .css file for the add pages

--- views/add_song.php
+++ views/add_song.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Song</title>
+    <link rel="stylesheet" href="./assets/add_form.css" />
 </head>
 <body>
     <nav class="navMenu">
